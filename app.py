f586ab5b9dabf59e2a43df8b6bb0c7e99ce2b499
shagun9999 <110155117+shagun9999@users.noreply.github.com>
2022-08-07 12:03:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model_rf = pickle.load(open('commentnlp.pkl','rb')) 
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    Review = request.args.get('Review')
+    input_data = [Review] 
+    input_data = cv.transform(input_data).toarray()
+    input_pred = model_rf.predict(input_data)
+    input_pred = input_pred.astype(int)
+    print(input_pred)
+
+
+    if input_pred[0]==1:
+      prediction_text="Review is Positive"
+    else:
+      prediction_text="Review is Negative"
+    return render_template('index.html', prediction_text=prediction_text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
